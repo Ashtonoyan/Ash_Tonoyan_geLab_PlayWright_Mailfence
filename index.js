74f4dfad113f8b1aad4963b186ab65064813e1db
Ash <atv007ash@gmail.com>
2025-02-09 20:33:19 +0400

    Saving document

--- index.js
+++ index.js
@@ -61,9 +61,31 @@ import {chromium} from "playwright";
     const fileInput = await page.locator('input[type="file"]');
 
     await fileInput.setInputFiles('C:/Users/atv00/Downloads/Mailfence e2e UI test case - TestRail (1) (2).pdf');
+    await page.waitForTimeout(5000);
+    await page.click('#mailSend')
 
-
+    await page.click('#dialBtn_YES')
 
     await page.waitForTimeout(10000);
+
+    await page.click('#treeInbox')
+
+    await page.waitForTimeout(10000);
+
+    const messages = await page.$$('.listSubject');
+    await messages[0].click();
+
+    await page.waitForTimeout(5000);
+    await page.click('a.GCSDBRWBJRB', { button: 'right' });
+    await page.waitForTimeout(5000);
+    await page.click('span.GCSDBRWBGR >> text="Сохранить в документах"');
+    await page.waitForTimeout(5000);
+    await page.click('div.treeItemLabel >> text="Мои документы"');
+    await page.waitForTimeout(2000);
+    await page.click('#dialBtn_OK')
+    await page.waitForTimeout(5000);
+    await page.click('.icon24-Documents.toolImg')
+    await page.waitForTimeout(5000);
+
     await browser.close();
 }) ();
