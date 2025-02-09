a9692edb0756fe6409b475f098143118cbf022cc
Ash <atv007ash@gmail.com>
2025-02-09 18:06:43 +0400

    Succes authorization

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ import {chromium} from "playwright";
 
     await page.waitForURL('https://mailfence.com/sw?type=L&state=0&lf=mailfence')
 
+    await page.waitForTimeout(1000);
+
+    await page.fill('#UserID', 'ashtonoyan@mailfence.com')
+    await page.fill('#Password', '123456789.Ash')
     await page.waitForTimeout(5000);
+    await page.click('input.btn[type="submit"]')
+    await page.waitForTimeout(5000);
+    await page.waitForURL('https://mailfence.com/flatx/index.jsp?v=2.8.028#home')
+
 
     await browser.close();
 }) ();
