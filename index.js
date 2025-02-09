b665ef0d59f9f58b587962bd801cbc729cb448e9
Ash <atv007ash@gmail.com>
2025-02-09 19:21:46 +0400

    Pasted the text into the field

--- index.js
+++ index.js
@@ -18,10 +18,40 @@ import {chromium} from "playwright";
 
     await page.fill('#UserID', 'ashtonoyan@mailfence.com')
     await page.fill('#Password', '123456789.Ash')
-    await page.waitForTimeout(5000);
+    //await page.waitForTimeout(5000);
     await page.click('input.btn[type="submit"]')
+    console.log("Кнопка нажата")
     await page.waitForTimeout(5000);
-    await page.waitForURL('https://mailfence.com/flatx/index.jsp?v=2.8.028#home')
+
+
+    await page.click('.icon24-Message.toolImg')
+    await page.waitForTimeout(1000);
+    await page.waitForURL('https://mailfence.com/flatx/index.jsp?v=2.8.028#tool=mail&folderoid=639842178')
+
+
+    const test = await page.waitForSelector('#mailNewBtn', { state: 'visible' });
+    if (test) {
+        console.log("Is visible");
+        await page.click('#mailNewBtn');
+    } else {
+        console.log("Is hidden");
+    }
+
+
+    await page.fill('input.GCSDBRWBPL[type="text"]', 'ashtonoyan@mailfence.com')
+    await page.waitForTimeout(5000);
+
+
+    const iframeElement = await page.waitForSelector('iframe.editable');
+
+
+    const frame = await iframeElement.contentFrame();
+
+    await frame.fill('#gwt-uid-32', 'ashtonoyan@mailfence.com')
+
+
+
+    await page.waitForTimeout(5000);
 
 
     await browser.close();
